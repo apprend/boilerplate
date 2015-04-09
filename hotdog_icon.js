5bb48d3aec2e7f67695b7e7ebe7ae54d94554fb8
Alan Languirand <alan@13protons.com>
2015-04-09 16:06:33 -0400

    snippet for leaflet custom icon

---
+++ hotdog_icon.js
@@ -0,0 +1,10 @@
+var hotdogMarker = L.icon({
+          iconUrl: 'https://raw.githubusercontent.com/apprend/boilerplate/master/images/hotdog-marker.png',
+          shadowUrl: 'https://raw.githubusercontent.com/apprend/boilerplate/master/images/hotdog-marker-shadow.png',
+
+          iconSize:     [50, 50], // size of the icon
+          shadowSize:   [50, 26], // size of the shadow
+          iconAnchor:   [10, 40], // point of the icon which will correspond to marker's location
+          shadowAnchor: [0, 15],  // the same for the shadow
+          popupAnchor:  [25, -28] // point from which the popup should open relative to the iconAnchor
+      });
